29900a617cbfa82baf81f536ede116660b5ea0f4
KarenCh23 <karenchamand@hotmail.fr>
2023-11-29 11:06:13 +0100

    fix on login client secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,7 +41,7 @@ export const authOptions = {
     providers: [
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID,
-            clientSecret: process.env.SPOTIFY_SECRET,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
             authorization: LOGIN_URL
         }),
     ],
