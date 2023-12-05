68f7d0b2d528c1df14c4ab1b777315ceceb72c50
KarenCh23 <karenchamand@hotmail.fr>
2023-12-05 11:37:55 +0100

    creating playerAtom state

--- components/Poster.js
+++ components/Poster.js
@@ -1,6 +1,9 @@
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 
 export default function Poster({ track }) {
+
+    
+
   return (
     <div className="w-[260px] h-[360px] rounded-[50px] overflow-hidden relative text-white/80 cursor-pointer hover:scale-105 hover:text-white/100 transition duration-200 ease-out group mx-auto">
       <img
